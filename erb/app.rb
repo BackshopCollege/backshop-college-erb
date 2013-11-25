788c4b1c3a0cb56fd6792ae015f8b876b7246549
Thiago Dantas <thiago.teixeira.dantas@gmail.com>
2013-11-25 15:38:23 +0000

    add dynamic template

--- erb/app.rb
+++ erb/app.rb
@@ -2,10 +2,17 @@ require 'erb'
 
 class Template
   def self.index
-    '<html><body> STATIC TEMPLATE </body></html>'
+    '<html><title> <%= title %> </title></html>'
   end
+end
+
+
+def title
+  "Backshop College"
 end
 
 compiled = ERB.new(Template.index)
 
-puts compiled.result
+#Capture the scope and call erb with the bindings
+captured_bindings = binding
+puts compiled.result(captured_bindings)
