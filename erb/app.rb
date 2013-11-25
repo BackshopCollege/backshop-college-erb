3aeb6849c2500db2f382b8f20e5e0ff56a207e31
Thiago Dantas <thiago.teixeira.dantas@gmail.com>
2013-11-25 15:57:59 +0000

    dynamic partial

--- erb/app.rb
+++ erb/app.rb
@@ -7,7 +7,7 @@ class Template
         <body> 
           <span> User: <%= @user.name %> </span>
           <span> Email: <%= @user.email %> </span> 
-          <%=partial Template.index_partial %>
+          <%= partial Template.index_partial, { company: @user.company } %>
         </body>
       </html>
     HEREDOC
@@ -15,32 +15,43 @@ class Template
 
   def self.index_partial
     <<-HEREDOC
-    <span>Company: Linkedcare </span>
+    <span>Company: <%= company %> </span>
     HEREDOC
   end
 end
 
+require 'ostruct'
 class ApplicationController
   def render(template)
     ERB.new(template).result(binding)
   end
 
-  def partial(template)
-    ERB.new(template).result
+  def partial(template, locals = {})
+    ERB.new(template).result(build_context(locals).get_binding)
+  end
+
+  private
+  def build_context(locals)
+    context = OpenStruct.new(locals)
+    def context.get_binding
+      binding
+    end
+    context
   end
 end
 
 class User
-  attr_accessor :name, :email
-  def initialize(name, email)
+  attr_accessor :name, :email, :company
+  def initialize(name, email, company)
     @name = name
     @email = email
+    @company = company
   end
 end
 
 class UsersController < ApplicationController
   def index
-    @user = User.new('Thiago Dantas', 'thiago.chapa@gmail.com')
+    @user = User.new('Thiago Dantas', 'thiago.chapa@gmail.com', 'linkedcare')
     render Template.index
   end
 end
